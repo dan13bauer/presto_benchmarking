25e5401a5d5e83fe13a47e2c842072dcd31efcf6
SeanRooooney <54989389+SeanRooooney@users.noreply.github.com>
2025-09-23 11:25:26 +0200

    Nvidia schema (#6)
    * add query directory
    
    * UCX params
    
    * ported queries
    
    * check if nvidia
    
    * Switch to MR=async in run workers script.
    
    ---------
    
    Co-authored-by: Daniel Bauer <dnb@zurich.ibm.com>

--- bin/test_tpch.sh
+++ bin/test_tpch.sh
@@ -8,10 +8,17 @@ PRESTO_CLI_DIR=$1
 QUERY_LIST=$2
 COORDINATOR_PORT=$3
 SF_SCHEMA=$4
+QUERY_DIR=./tpch_queries
+
+if [[ "$SF_SCHEMA" == *"_nvidia" ]]; then
+    echo "using nvidia queries !"
+    QUERY_DIR=./tpch_queries_nvidia
+fi
 
 for query in $(cat $QUERY_LIST); do
 
-    file=./tpch_queries/$query    
+
+    file=$QUERY_DIR/$query    
     if [[ -f "$file" ]]; then
         echo "*** Executing query $file on schema $SF_SCHEMA"
 	$PRESTO_CLI_DIR/presto --server localhost:$COORDINATOR_PORT --catalog hive --schema $SF_SCHEMA --session single_node_execution_enabled=false -f $file 
