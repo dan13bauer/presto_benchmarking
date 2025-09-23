9c490b5ee16f7f797459c07bc4dba44ab26d6893
Daniel Bauer <dnb@zurich.ibm.com>
2025-09-23 11:24:46 +0200

    Switch to MR=async in run workers script.

--- bin/run_all_workers.sh
+++ bin/run_all_workers.sh
@@ -19,7 +19,7 @@ for GPU  in $(seq 0 7); do
     UCX_PARAMS="UCX_TCP_CM_REUSEADDR=y UCX_PROTO_INFO=y UCX_LOG_LEVEL=error UCX_RNDV_PIPELINE_ERROR_HANDLING=y"
     CUDA_PARAMS="CUDA_VISIBLE_DEVICES=$GPU GLOG_logtostderr=1"
     #VELOX_ARGS="-velox_cudf_debug=true -velox_cudf_table_scan=false -velox_cudf_enabled=false -velox_cudf_exchange=false -velox_cudf_memory_resource=pool -v=3"
-    VELOX_ARGS="-velox_cudf_debug=true -velox_cudf_table_scan=true -velox_cudf_enabled=true -velox_cudf_exchange=true -velox_cudf_memory_resource=pool -v=3"
+    VELOX_ARGS="-velox_cudf_debug=true -velox_cudf_table_scan=true -velox_cudf_enabled=true -velox_cudf_exchange=true -velox_cudf_memory_resource=async -v=3"
     WORKER_ID=$((GPU+1))
     
     env $UCX_PARAMS env $CUDA_PARAMS $BUILD_DIR/presto_cpp/main/presto_server  -etc_dir $WORKERS_DIR/worker_$WORKER_ID/etc $VELOX_ARGS &
