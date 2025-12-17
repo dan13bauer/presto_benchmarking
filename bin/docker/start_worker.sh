467f3d729cdcd76317a4a7d5b6b944aad518ee56
Sean Rooney <sro@zurich.ibm.com>
2025-12-17 11:26:32 +0100

    allow rm container

--- bin/docker/start_worker.sh
+++ bin/docker/start_worker.sh
@@ -8,27 +8,24 @@ else
 
 WORKER_ID=$1
 GPU_ID=$((WORKER_ID-1))
-IMG_BASE=presto/unified-76f7902
+IMG_BASE=presto-a3141860dc/velox-93c21a2b6
+#IMG_BASE=presto/main-dec2-drv-q9-fix
 #IMG_BASE=471112500371.dkr.ecr.us-west-2.amazonaws.com/presto/prestissimo-runtime-centos9
 #IMG_VER=ucx1.19.0-latest-exchange
 #IMG_VER=ucx1.19.0-latest-exchange-mem-cudf2510
 #IMG_VER=latest-rapidsai-exchange
+#IMG_VER=9d198e0
 IMG_VER=centos
 
 IMG=${IMG_BASE}:${IMG_VER}
 DEV_ARG=\"device=${GPU_ID}\"
 
-#V_ARGS="-velox_cudf_debug=true -velox_cudf_table_scan=true -velox_cudf_enabled=true -velox_cudf_exchange=true -velox_cudf_memory_resource=async -v=3"
-
-#V_ARGS="-velox_cudf_debug=true -velox_cudf_table_scan=true -velox_cudf_enabled=true -velox_cudf_exchange=true -velox_cudf_memory_resource=async -velox_cudf_memory_percentage=90 -v=3"
-#V_ARGS="-velox_cudf_debug=false -velox_cudf_table_scan=false -velox_cudf_enabled=false -v=3"
-#V_ARGS="-velox_cudf_debug=false -velox_cudf_enabled=true -velox_cudf_memory_resource=async  -v=3"
-
-#V_ARGS="-velox_cudf_debug=true -velox_cudf_enabled=true -velox_cudf_exchange=false -velox_cudf_memory_resource=async -v=3"
-#V_ARGS="-velox_cudf_debug=true -velox_cudf_enabled=true -velox_cudf_exchange=true -velox_cudf_memory_resource=async -v=3"
 V_ARGS="-v=3"
 
-docker run --rm -d  --gpus all -it --network=host --cap-add=IPC_LOCK --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --pid host \
+#RM_CMD="--rm"
+RM_CMD=""
+
+docker run $RM_CMD -d  --gpus all -it --network=host --cap-add=IPC_LOCK --shm-size=1g --ulimit memlock=-1 --ulimit stack=67108864 --pid host \
        --runtime=nvidia \
        --device /dev/infiniband/rdma_cm  --device=/dev/infiniband/uverbs0 --device=/dev/infiniband/uverbs1 --device=/dev/infiniband/uverbs2 --device=/dev/infiniband/uverbs3 --device=/dev/infiniband/uverbs4 --device=/dev/infiniband/uverbs5 --device=/dev/infiniband/uverbs6 --device=/dev/infiniband/uverbs7 --device=/dev/infiniband/uverbs8 --device=/dev/infiniband/uverbs9 \
        -e KVIKIO_COMPAT_MODE=OFF \
